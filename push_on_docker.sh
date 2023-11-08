1a2b5f277aefa76e92d7683041d79f418051c25e
Luca Framba <framba@fbk.eu>
2023-11-08 14:05:52 +0100

    Added logos and modified docker location

--- push_on_docker.sh
+++ push_on_docker.sh
@@ -1,2 +1,2 @@
-sudo docker build -t frambaluca/ai4eu-experiments:tsb-space-v3 .
-sudo docker push frambaluca/ai4eu-experiments:tsb-space-v3
+sudo docker build -t frambaluca/ai4eu-experiments:tsb-space .
+sudo docker push frambaluca/ai4eu-experiments:tsb-space
